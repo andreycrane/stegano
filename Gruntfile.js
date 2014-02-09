3fd51c3b07bd5fc980712a94ea62d77f606017ec
Andrey Crane <crane.andrey@gmail.com>
2014-02-09 22:16:56 +0300

    Добавлено задание компиляции стилей в Grunfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,37 @@ module.exports = function(grunt) {
     'use strict';
     
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+        pkg: grunt.file.readJSON('package.json'),
+        
+        less: {
+            style: {
+                options: {
+                    cleancss: true
+                },
+                
+                files: {
+                    "css/dest/styles.min.css": "css/src/styles.less"
+                }
+            }
+        },
+        
+        watch: {
+            grunt: {
+                files: ['Gruntfile.js']
+            },
+            
+            styles: {
+                files: ['css/src/*.less'],
+                tasks: ['less']
+            }
+        }
     });
-};    
+    
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    
+    
+    grunt.registerTask('default', ['less'])
+};
