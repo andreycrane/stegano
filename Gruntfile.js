784846b5b7d7c0ed70ccd8d0a0406f132fdcc17a
Andrey Crane <crane.andrey@gmail.com>
2014-03-02 20:35:18 +0300

    Добавлен файл манифеста кэша приложения

--- Gruntfile.js
+++ Gruntfile.js
@@ -25,12 +25,25 @@ module.exports = function(grunt) {
         
         watch: {
             grunt: {
-                files: ['Gruntfile.js']
+                files: ['Gruntfile.js'],
+                tasks: ['manifest']
             },
             
             styles: {
                 files: ['css/src/*.less'],
-                tasks: ['less:style', 'copy:styles']
+                tasks: ['less:style', 'copy:styles', 'manifest']
+            },
+            
+            other_files: {
+                files: [
+                    'js/*.js',
+                    'js/views/*.js',
+                    'locales/*/*.json',
+                    'static/*/*.html',
+                    'templates/*.html'
+                ],
+                
+                tasks: ['manifest']
             }
         }
     });
@@ -41,5 +54,76 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     
     
-    grunt.registerTask('default', ['less:style', 'copy:styles'])
+    grunt.registerTask('default', ['less:style', 'copy:styles']);
+    
+    grunt.registerTask('manifest',
+                       'Generate HTML5 application cache manifest file',
+                       function () {
+        
+        var manifest_template,
+            manifest_date,
+            manifest_text;
+        
+        manifest_date = grunt.template.today('dd-mm-yyyy HH:MM:ss');
+        
+        manifest_template = [
+            "CACHE MANIFEST",
+            "# <%= manifest_date %>",
+            "index.html",
+            // CSS
+            "css/styles.min.css",
+            "css/bootstrap.min.css",
+            // JavaScript
+            "js/lib/backbone.min.js",
+            "js/lib/bootstrap.min.js",
+            "js/lib/i18next.min.js",
+            "js/lib/jquery.min.js",
+            "js/lib/require.min.js",
+            "js/lib/underscore.min.js",
+            "js/lib/backbone-min.map",
+            "js/lib/underscore-min.map",
+            "js/views/application.js",
+            "js/views/static.js",
+            "js/app.js",
+            "js/stegano.js",
+            "js/text.js",
+            // fonts
+            "fonts/glyphicons-halflings-regular.eot",
+            "fonts/glyphicons-halflings-regular.svg",
+            "fonts/glyphicons-halflings-regular.ttf",
+            "fonts/glyphicons-halflings-regular.woff",
+            // locales
+            "locales/dev/translation.json",
+            "locales/en/translation.json",
+            "locales/ru/translation.json",
+            "locales/ua/translation.json",
+            // Static pages
+            "/static/about/about_en.html",
+            "/static/about/about_ru.html",
+            "/static/about/about_ua.html",
+            "/static/help/help_en.html",
+            "/static/help/help_ua.html",
+            "/static/help/help_ru.html",
+            // templates
+            "/templates/upload_template.html",
+            // images
+            "/img/help/1.png",
+            "/img/help/2.png",
+            "/img/help/3.png",
+            "/img/help/4.png",
+            "/img/help/5.png",
+            "NETWORK:",
+            "*"
+        ].join('\n');
+        
+        manifest_text = grunt.template.process(manifest_template, {
+            data: {
+                manifest_date: manifest_date
+            }
+        });
+        
+        grunt.file.write('cache.manifest', manifest_text);
+        
+        grunt.log.ok('Manifest file created with date ' + manifest_date);
+    });
 };
